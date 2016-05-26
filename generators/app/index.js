14bfb549e86211cfa90303922be14332cd0f681a
Thang <thang.coder@yandex.com>
2016-05-26 14:41:54 -0400

    Add an archived version of Yeoman’s tutorial

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,7 +7,7 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the finest ' + chalk.red('generator-live-server-demo') + ' generator!'
+      'Welcome to the ' + chalk.red('live-server demo') + ' generator!'
     ));
 
     var prompts = [{
@@ -16,18 +16,24 @@ module.exports = yeoman.Base.extend({
       message: 'Would you like to launch live-server at the end?',
       default: true
     }];
+    
+    var skipLaunch = this.log.bind(this, 'Skipping live-server');
+    var launch = this.spawnCommand.bind(this, 'live-server');
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
+      this.on('end',props.shouldLaunchLiveServer ? launch : skipLaunch);
+      
     }.bind(this));
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    // this.fs.copy(
+    //   this.templatePath('dummyfile.txt'),
+    //   this.destinationPath('dummyfile.txt')
+    // );
+    this.bulkDirectory('writing-tutorial', 'tutorial');
   },
 
   install: function () {
